1b0625884d1a4b4438b5be89496ac8aeac99a750
Van Ortega <ortegavan@live.com>
2021-11-04 04:06:54 -0300

    ajustes de responsividade tela pp

--- code/js/main.js
+++ code/js/main.js
@@ -48,8 +48,24 @@ function exibirCookies() {
     }
 }
 
+function exibirMenuResponsivo() {
+    $("nav").removeClass("oculto-pp").addClass("menu-responsivo");
+    $(".menu-responsivo-fechar").removeClass("oculto-pp");
+}
+
+function ocultarMenuResponsivo() {
+    $("nav").removeClass("menu-responsivo").addClass("oculto-pp");
+    $(".menu-responsivo-fechar").addClass("oculto-pp");
+}
+
 function buscar() {
     let busca = $("#busca").val();
+    localStorage.setItem("busca", busca);
+    window.location.href = "busca.html";
+}
+
+function buscarVersaoMobile() {
+    let busca = $("#busca-mobile").val();
     localStorage.setItem("busca", busca);
     window.location.href = "busca.html";
 }
@@ -135,7 +151,7 @@ function renderizarFavoritos(selecionados) {
             Meus favoritos
         </h2>`;
 
-    if (selecionados.length > 0){
+    if (selecionados.length > 0) {
         for (let produto of selecionados) {
             template += `
             <div class="margem-topo">
@@ -167,7 +183,7 @@ function renderizarCarrinho(selecionados) {
             Minha cesta de compras
         </h2>`;
 
-    if (selecionados.length > 0){
+    if (selecionados.length > 0) {
         for (let produto of selecionados) {
             template += `
             <div class="margem-topo">
@@ -189,7 +205,7 @@ function renderizarCarrinho(selecionados) {
 
     template += `
         <p class="margem-topo">&nbsp;</p>
-        <a href="index.html" class="carrinho-voltar">
+        <a href="index.html" class="carrinho-voltar oculto-pp">
             <i class="far fa-arrow-left"></i> continuar comprando
         </a>
         <button>
@@ -310,7 +326,10 @@ function renderizarRelacionados(produto, relacionados) {
 }
 
 function renderizarBuscados() {
-    let busca = localStorage.getItem("busca").toLowerCase();
+    let busca = localStorage.getItem("busca");
+
+    busca = busca != null ? busca.toLowerCase() : "";
+
     let buscados = produtos.filter(p =>
         p.titulo.toLowerCase().indexOf(busca) >= 0 ||
         p.descricao.toLowerCase().indexOf(busca) >= 0);
